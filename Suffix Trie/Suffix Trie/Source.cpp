3dee7bdeb8fa8104daaa5b9975144e8e4f9e7155
Youssef Ahmed <youssef9300@hotmail.com>
2021-01-14 06:28:34 +0200

    construction insertion function

--- Suffix Trie/Suffix Trie/Source.cpp
+++ Suffix Trie/Suffix Trie/Source.cpp
@@ -1,22 +1,144 @@
 #include <iostream>
 using namespace std;
 
-class trieNode {
-public:
-	linkedList list;
-};
+
 
 class linkedlistNode {
 public:
+
 	char value;
 	linkedlistNode* next;
-	trieNode* child;
+	linkedlistNode** child;
+
+	linkedlistNode() {
+		this->next = NULL;
+		this->child = NULL;
+	}
 
 };
 
 class linkedList {
+
 public:
-	linkedlistNode* root;
+	linkedlistNode** root;
+	int size = 0;
+};
+
+
+class SuffixTrie {
+	linkedList arr;
+
+public:
+
+	SuffixTrie(){}
+
+	SuffixTrie(string input) {
+		if (arr.root == NULL) {
+			arr.root = new linkedlistNode*;
+		}
+
+		for (int i = 1; i <= input.size(); i++) {
+			firstInsert(input, i);
+		}
+	}
+
+	//Check if Initial Char in the sub string exists and if not it creates a node that starts with it
+	void firstInsert(string input, int index) {
+		bool exists = false;
+		int at = 0;
+
+		for (int i = 0; i < arr.size; i++) {
+			if (arr.root[i]->value == input[input.size() - index]) {
+				exists = true;
+				at = i;
+				break;
+			}
+		}
+
+		if (exists == false) {
+			arr.root[arr.size] = new linkedlistNode;
+			arr.root[arr.size]->value = input[input.length() - index];
+			arr.root[arr.size]->child = NULL;
+			arr.root[arr.size]->next = NULL;
+			insert(arr.root[arr.size], input, index - 1);
+			arr.size++;
+		}
+		else if (exists == true) {
+			insert(arr.root[at], input, index - 1);
+		}
+
+		
+	}
+
+
+	void insert(linkedlistNode* Node, string input, int index) {
+		//Base Case to stop the reccursive function
+		if (index == 0) {
+			return;
+		}
+
+		cout << "inserting: " << input[input.size() - index] << endl;
+
+		if (Node == NULL) {
+			Node = new linkedlistNode;
+			Node->value = input[input.size() - index];
+			insert(Node->next, input, index - 1);
+		}
+		else if (Node->next == NULL) {
+			Node->next = new linkedlistNode;
+			Node->next->value = input[input.size() - index];
+			insert(Node->next, input, index - 1);
+		}
+		else if (Node->child == NULL) {
+			
+			Node->child = new linkedlistNode*;
+			Node->child[0] = new linkedlistNode;
+			Node->child[0]->value = input[input.length() - index];
+			Node->child[0]->next = NULL;
+			Node->child[0]->child = NULL;
+			insert(Node->child[0], input, index - 1);
+		}
+		else {
+			linkedlistNode* temp = checkChild(Node->child, input, index);
+			if (temp == NULL) {
+			insert(Node, input, index - 1);
+			}
+			else
+			{
+				insert(Node->next, input, index - 1);
+			}
+		}
+		
+	}
+
+	linkedlistNode* checkChild(linkedlistNode** Node, string input, int index) {
+
+		int i = 0;
+
+		if (Node != NULL) {
+			while (Node[i] != NULL) {
+				if (Node[i]->value == input[input.size() - index]) {
+					return Node[i];
+				}
+				i++;
+			}
+		}
+
+		return Node[i];
+
+	}
+
+	void print() {
+		/*linkedlistNode* temp;
+		for (int i = 0; i < arr.size; i++) {
+			temp = arr.root[i]->next;
+			cout << arr.root[i]->value;
+			
+		}*/
+		cout << endl;
+		cout << arr.root[1]->value << arr.root[1]->next->value  << endl;
+		//cout << arr.root[1]->child[0]->value << arr.root[1]->child[0]->next->value << endl;
+	}
 };
 
 
@@ -26,7 +148,8 @@ int main()
     // Construct a suffix trie containing all suffixes of "bananabanaba$"
 
     //            0123456789012
-   // SuffixTrie t("bananabanaba$");
+    SuffixTrie t("bana$");
+	t.print();
 
    // t.Search("ana"); // Prints: 1 3 7
     //t.Search("naba"); // Prints: 4 8
